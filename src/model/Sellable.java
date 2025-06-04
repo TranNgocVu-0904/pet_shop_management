1a89509a5590c303368c6bbbb9a3d1e78b3feae5
HoangVu <bvhoangvu@gmail.com>
2025-06-04 23:37:30 +0700

    refactor: reorganize project structure

--- src/model/Sellable.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-import java.math.BigDecimal;
-
-public interface Sellable {
-    String getDisplayName();     // For invoices, UI, etc.
-    BigDecimal getPrice();       // For billing totals
-}
--- src/view/RowMapper.java
+++
@@ -1,7 +0,0 @@
-package view;
-
-import javax.swing.table.DefaultTableModel;
-
-public interface RowMapper<T> {
-    T mapRow(DefaultTableModel model, int row);
-}
